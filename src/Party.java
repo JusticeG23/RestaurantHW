b9d347e787a830270a48c973ff42afe498de541d
Sarah Au <sarahelizabeth.au@calbaptist.edu>
2022-03-10 21:18:11 -0800

    add Party properties and methods

--- src/Party.java
+++ src/Party.java
@@ -1,2 +1,48 @@
 public class Party {
+    private int partySize;
+    private String partyName;
+    private boolean isSeated;
+    private double billSubtotal;
+
+    public Party(int size, String name) {
+        this.partySize = size;
+        this.partyName = name;
+        this.isSeated = false;
+        this.billSubtotal = 0.0;
+    }
+
+    // Getters below:
+
+    /**
+     * Gets the Party size
+     * @return the int of the party size
+     */
+    public int getPartySize() {
+        return partySize;
+    }
+
+    /**
+     * Gets the Party name
+     * @return the String of the party name
+     */
+    public String getPartyName() {
+        return partyName;
+    }
+
+    /**
+     * Returns true if the Party is seated (in the Restaurant at a Table and not on a waitlist),
+     * false if the Party is not seated (in the waitlist)
+     * @return boolean if the party is seated or not
+     */
+    public boolean isSeated() {
+        return isSeated;
+    }
+
+    /**
+     * Gets the subtotal of the bill for the Party
+     * @return double the subtotal
+     */
+    public double getBillSubtotal() {
+        return billSubtotal;
+    }
 }
