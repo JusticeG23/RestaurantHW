9b94eb46f5bf353f286a12d11b66273970bc9976
Sarah Au <sarahelizabeth.au@calbaptist.edu>
2022-03-10 19:38:32 -0800

    add Restaurant properties and methods

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,2 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Restaurant {
+    private List<Table> tableList;
+    private List<Server> serverList;
+    private List<Party> waitlist;
+    private double cashRegister;
+
+    /**
+     * Constructor
+     */
+    public Restaurant() {
+        this.tableList = new ArrayList<>();
+        this.serverList = new ArrayList<>();
+        this.waitlist = new ArrayList<>();
+        this.cashRegister = 0.0;
+    }
+
+    public void setUpRestaurant() {
+        // TODO add tables based on txt file
+    }
+
+    public void addParty() {
+        // TODO add a Party to the waitlist
+    }
+
+
+    public void removeParty() {
+        // TODO remove a Party from the waitlist
+    }
+
+
+    // Getters below:
+    /**
+     * Gets the current Table list
+     * @return the list of tables
+     */
+    public List<Table> getTableList() {
+        return tableList;
+    }
+
+    /**
+     * Gets the current Server list
+     * @return the list of servers
+     */
+    public List<Server> getServerList() {
+        return serverList;
+    }
+
+    /**
+     * Gets the current waitlist of Parties
+     * @return the list of parties
+     */
+    public List<Party> getWaitlist() {
+        return waitlist;
+    }
+
+    /**
+     * Gets the current amount of money in the cash register
+     * @return the amount of money in the cash register
+     */
+    public double getCashRegister() {
+        return cashRegister;
+    }
 }
